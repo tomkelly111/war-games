9ea387bf2d13a0126b6e16a240af9b89a8a376e9
tomkelly111 <thomaspkelly111@gmail.com>
2022-12-03 16:08:18 +0000

    Add code to gameSelector function to read users choice and start appropriate game

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,22 +8,37 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
-
+/**This function takes the users chosen weaponType and runs the appropriate game,
+ * If random is chosen then a random number is generated and the appropriate game is selected.
+ * 
+ */
 function gameSelector(weaponType) {
-
+    if (weaponType === "0") {
+        rockGame();
+    } else if (weaponType === "1") {
+        paperGame();
+    } else if (weaponType === "2") {
+        scissorsGame();
+    } else if (weaponType === "3") {
+        let randomWeapon = Math.floor(Math.random() * 3);
+        let weaponType = randomWeapon.toString();
+        gameSelector(weaponType);
+    } let computerChoice = document.getElementById("computer-choice");
+    computerChoice.innerHTML = `<i class="fa-regular fa-circle-question"></i>`;
 }
 
 function rockGame() {
-
+    console.log("rockGame success")
 }
 
 function paperGame() {
-
+    console.log("paperGame success")
 }
 
 function scissorsGame() {
-
+    console.log("scissorsGame success")
 }
+
 
 
 function startGame() {
