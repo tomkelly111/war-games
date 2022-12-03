e4b565e4bd47880d4d4435f830d8864a9b5281ee
tomkelly111 <thomaspkelly111@gmail.com>
2022-12-03 11:37:49 +0000

    add event listner to buttons once DOM content is loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("Connected!")
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            console.log("addEvent listner success")
+        });
+    }
+});
